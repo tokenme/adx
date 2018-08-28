a6e7220522709efe62d6a5c34f2db63885a89898
Ted <824696468@qq.com>
2018-08-28 15:58:17 +0800

    Add Phone Email Intro ....

--- handler/media/Info_admin.go
+++ handler/media/Info_admin.go
@@ -8,6 +8,46 @@ import (
 	"time"
 )
 
+func IndexMediaHander(c *gin.Context){
+	userContext, exists := c.Get("USER")
+	if Check(!exists, "need login", c) {
+		return
+	}
+	user := userContext.(common.User)
+	if Check(user.IsAdmin!=1,"is not admin",c){
+		return
+	}
+	db := Service.Db
+	id := c.Param("id")
+	row,resut,err:=db.Query(`SELECT a.id,a.user_id,t.mobile,t.email,
+	a.title,a.domain,a.intro,a.verified,a.online_status,a.verified_at,
+	a.inserted_at,a.updated_at FROM adx.medias AS a 
+	INNER JOIN adx.users AS t ON (a.user_id=t.id) 
+	WHERE a.id=%s `,id)
+	CheckErr(err,c)
+	if len(row) == 0{
+		c.JSON(http.StatusNotFound,"Not Find Media")
+		return
+	}
+	Media := common.Media{
+		Id:           row[0].Uint64(resut.Map("id")),
+		UserId:       row[0].Uint64(resut.Map("user_id")),
+		Title:        row[0].Str(resut.Map("title")),
+		Domain:       row[0].Str(resut.Map("domain")),
+		Intro:		  row[0].Str(resut.Map("intro")),
+		Verified:     row[0].Uint(resut.Map("verified")),
+		OnlineStatus: row[0].Uint(resut.Map("online_status")),
+		Mobile: 	  row[0].Str(resut.Map("mobile")),
+		Email: 		  row[0].Str(resut.Map("email")),
+		Verified_at:  row[0].Time((resut.Map("verified_at")), time.Local),
+		InsertedAt:   row[0].Time((resut.Map("inserted_at")), time.Local),
+		UpdatedAt:    row[0].Time(resut.Map("updated_at"), time.Local),
+	}
+
+	c.JSON(http.StatusOK,Media)
+}
+
+
 func MediaInfoHandler(c *gin.Context){
 	userContext, exists := c.Get("USER")
 	if Check(!exists, "need login", c) {
@@ -18,7 +58,10 @@ func MediaInfoHandler(c *gin.Context){
 		return
 	}
 	db := Service.Db
-	row,resut,err:=db.Query(`SELECT id,user_id,title,intro,online_status,inserted_at,updated_at FROM medias`)
+	row,resut,err:=db.Query(`SELECT a.id,a.user_id,t.mobile,t.email,
+	a.title,a.domain,a.intro,a.verified,a.online_status,a.verified_at,
+	a.inserted_at,a.updated_at FROM adx.medias AS a 
+	INNER JOIN adx.users AS t ON (a.user_id=t.id)`)
 	CheckErr(err,c)
 	info:=[]common.Media{}
 	for _,value:=range row{
@@ -26,14 +69,17 @@ func MediaInfoHandler(c *gin.Context){
 			Id:           value.Uint64(resut.Map("id")),
 			UserId:       value.Uint64(resut.Map("user_id")),
 			Title:        value.Str(resut.Map("title")),
+			Domain:       value.Str(resut.Map("domain")),
+			Intro:		  value.Str(resut.Map("intro")),
+			Verified:     value.Uint(resut.Map("verified")),
 			OnlineStatus: value.Uint(resut.Map("online_status")),
+			Mobile: 	  value.Str(resut.Map("mobile")),
+			Email: 		  value.Str(resut.Map("email")),
+			Verified_at:  value.Time((resut.Map("verified_at")), time.Local),
 			InsertedAt:   value.Time((resut.Map("inserted_at")), time.Local),
 			UpdatedAt:    value.Time(resut.Map("updated_at"), time.Local),
 		}
 		info = append(info, Media)
-		if err !=nil{
-			break
-		}
 	}
 	c.JSON(http.StatusOK,info)
 }
